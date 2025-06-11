c062bd10d90dad317113db0a0091dcdc63cf3fd5
Chanbormei Sun <sunchanbormeysunchanbormey@gmail.com>
2025-06-11 19:21:43 +0700

    commit to GitHub1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "✅ Hello, world! This Flask app is running in Docker."
+    return "✅ Hello, world! This Flask app is running in Docker. My name is SUN CHANBORMEI"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
